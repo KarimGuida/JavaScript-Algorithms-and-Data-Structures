263a708938c279680d4ca019fa3c8860708ec084
karim <guida.karim@gmail.com>
2024-02-22 14:08:10 +0100

    escape sequences in strings

---
+++ Basic JavaScript/27-escape sequences in strings.js
@@ -0,0 +1,14 @@
+//Quotes are not the only characters that can be escaped inside a string. Escape sequences allow us to use characters we may not otherwise be able to use in a string.
+
+//   Code	Output
+//    \'	single quote
+//    \"	double quote
+//    \\	backslash
+//    \n	newline
+//    \t	tab
+//    \r	carriage return
+//    \b	backspace
+//    \f	form feed
+
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
