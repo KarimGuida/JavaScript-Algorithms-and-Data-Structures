8aacf22b347fc93b350349ae98da8deacb3eaf0a
karim <guida.karim@gmail.com>
2024-02-22 14:04:23 +0100

    quoting strings with single quotes

---
+++ Basic JavaScript/26-quoting strings with single quotes.js
@@ -0,0 +1,6 @@
+//Unlike some other programming languages, single and double quotes work the same in JavaScript.
+
+const doubleQuoteStr = "This is a string"; 
+const singleQuoteStr = 'This is also a string';
+
+const conversation = 'Finn exclaims to Jake, "Algebraic!"';
