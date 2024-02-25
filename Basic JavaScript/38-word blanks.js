ad7d6c61f53aa3e2ac5f554eb28cb3a2f6e97aff
karim <guida.karim@gmail.com>
2024-02-25 20:36:19 +0100

    word blanks

---
+++ Basic JavaScript/38-word blanks.js
@@ -0,0 +1,6 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My " + myNoun + " is " + myAdjective + " and it " + myVerb + " " + myAdverb + ".";
