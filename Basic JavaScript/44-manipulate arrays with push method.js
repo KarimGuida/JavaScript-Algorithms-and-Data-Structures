80fb9d723e975b811ab9f577181cc5ff94b7480d
karim <guida.karim@gmail.com>
2024-02-27 15:52:06 +0100

    manipulate arrays with push method

---
+++ Basic JavaScript/44-manipulate arrays with push method.js
@@ -0,0 +1,5 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// The push() method takes one or more arguments and appends them to the end of the array, in the order in which they appear. It returns the new length of the array.
+myArray.push(["dog", 3]);
