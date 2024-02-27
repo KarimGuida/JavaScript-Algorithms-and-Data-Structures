39ac72ef504228a94d49989c8b901b61def9f219
karim <guida.karim@gmail.com>
2024-02-27 15:54:50 +0100

    manipulate arrays with pop method

---
+++ Basic JavaScript/45-manipulate arrays with pop method.js
@@ -0,0 +1,7 @@
+// .pop() is used to pop a value off of the end of an array. We can store this popped off value by assigning it to a variable.
+// In other words, .pop() removes the last element from an array and returns that element.
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+let removedFromMyArray = myArray.pop();
