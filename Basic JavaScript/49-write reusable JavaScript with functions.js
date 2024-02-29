48595161d0a0403a9c91f63bc651c244169bc32c
karim <guida.karim@gmail.com>
2024-02-29 15:45:54 +0100

    write reusable JavaScript with functions

---
+++ Basic JavaScript/49-write reusable JavaScript with functions.js
@@ -0,0 +1,9 @@
+// In JavaScript, we can divide up our code into reusable parts called functions.
+
+function reusableFunction(){
+    console.log("Hello World");
+  }
+  
+// We can call or invoke this function by using its name followed by parentheses, like this:
+
+reusableFunction();
