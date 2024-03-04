1d14d84019e22adc71d4de19409eb2511afb6db6
karim <guida.karim@gmail.com>
2024-03-04 16:42:45 +0100

    understanding Boolean values

---
+++ Basic JavaScript/58-understanding Boolean values.js
@@ -0,0 +1,6 @@
+// Another data type is the Boolean. Booleans may only be one of two values: true or false.
+// They are basically little on-off switches, where true is on and false is off. These two states are mutually exclusive.
+
+function welcomeToBooleans() {
+    return true; 
+  }
