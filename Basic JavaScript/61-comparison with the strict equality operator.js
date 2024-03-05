571c56fbb57e09305ef5b038651f1aaeb4ba5ecd
karim <guida.karim@gmail.com>
2024-03-05 15:07:31 +0100

    comparison with the strict equality operator

---
+++ Basic JavaScript/61-comparison with the strict equality operator.js
@@ -0,0 +1,17 @@
+//Strict equality (===) is the counterpart to the equality operator (==).
+//However, unlike the equality operator, which attempts to convert both values being compared to a common type,
+// the strict equality operator does not perform a type conversion.
+
+//Examples
+
+3 ===  3  // true
+3 === '3' // false
+
+function testStrict(val) {
+    if (val === 7) {
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
