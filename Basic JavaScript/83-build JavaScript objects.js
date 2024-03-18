2a93c399b42d8526db4d92fbd729d523864f2fb9
karim <guida.karim@gmail.com>
2024-03-18 20:19:11 +0000

    build JavaScript objects

---
+++ Basic JavaScript/83-build JavaScript objects.js
@@ -0,0 +1,9 @@
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data,
+// you access the data in objects through what are called properties.
+
+const myDog = {
+    "name": "doggy",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["A", "B", "C"]
+};
