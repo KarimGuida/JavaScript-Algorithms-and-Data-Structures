f3e811afe386f5f0cf522083cdb05996c897f452
karim <guida.karim@gmail.com>
2024-03-20 20:30:16 +0000

    updating object properties

---
+++ Basic JavaScript/87-updating object properties.js
@@ -0,0 +1,12 @@
+// After you've created a JavaScript object, 
+// you can update its properties at any time just like you would update any other variable.
+// You can use either dot or bracket notation to update.
+
+const myDog = {
+    "name": "Dog",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["other dogs"]
+};
+  
+myDog.name = "Happy Dog";
