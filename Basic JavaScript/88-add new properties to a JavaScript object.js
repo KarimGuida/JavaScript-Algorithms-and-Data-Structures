b4ff495097ead8283e8138ad5f41a9c1fb1aec52
karim <guida.karim@gmail.com>
2024-03-21 10:48:10 +0000

    add new properties to a JavaScript object

---
+++ Basic JavaScript/88-add new properties to a JavaScript object.js
@@ -0,0 +1,10 @@
+// You can add new properties to existing JavaScript objects the same way you would modify them.
+
+const myDog = {
+    "name": "Happy Dog",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["other dogs"]
+  };
+  
+  myDog.bark = "woof";
