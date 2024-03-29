56e343163ae90cea3296de90bf748812da486d0e
karim <guida.karim@gmail.com>
2024-03-29 20:10:11 +0000

    profile lookup

---
+++ Basic JavaScript/104-profile lookup.js
@@ -0,0 +1,57 @@
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    let isActualFirstName = false;
+    let isProperty = false;
+    let temp = 0;
+  
+    for (let i = 0; i < contacts.length; i++){
+      if (name == contacts[i].firstName){
+        isActualFirstName = true;
+        isProperty = contacts[i].hasOwnProperty(prop);
+        temp = i;
+        break;
+      }
+    }
+  
+    if (isActualFirstName === true && isProperty === true){
+      return contacts[temp][prop];
+    }
+  
+    else if (isActualFirstName === false){
+      return "No such contact";
+    }
+  
+    else if (isActualFirstName === true && isProperty === false){
+      return "No such property";
+    }
+  
+  
+}
+  
+lookUpProfile("Akira", "likes");
